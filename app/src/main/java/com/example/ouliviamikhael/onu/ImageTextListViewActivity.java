23011a0db1dad88d43b6a2dc98cb7e51ffcf00a6
ouliviamikhael <olivia.mikhael@gmail.com>
2015-08-18 13:25:39 -0400

    attepting to add listview function

---
+++ app/src/main/java/com/example/ouliviamikhael/onu/ImageTextListViewActivity.java
@@ -0,0 +1,65 @@
+package com.example.ouliviamikhael.onu;
+
+/**
+ * Created by ouliviamikhael on 15-08-13.
+ */
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.example.ouliviamikhael.onu.RowItem;
+import com.example.ouliviamikhael.onu.CustomListViewAdapter;
+
+public class ImageTextListViewActivity extends Activity implements
+        OnItemClickListener {
+
+    public static final String[] titles = new String[] { "Strawberry",
+            "Banana", "Orange", "Mixed" };
+
+    public static final String[] descriptions = new String[] {
+            "It is an aggregate accessory fruit",
+            "It is the largest herbaceous flowering plant", "Citrus Fruit",
+            "Mixed Fruits" };
+
+    public static final Integer[] images = { R.mipmap.ic_login,
+            R.mipmap.ic_launcher, R.mipmap.ic_main_menu, R.mipmap.ic_new_account };
+
+    ListView listView;
+    List<RowItem> rowItems;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rowItems = new ArrayList<RowItem>();
+        for (int i = 0; i < titles.length; i++) {
+            RowItem item = new RowItem(images[i], titles[i], descriptions[i]);
+            rowItems.add(item);
+        }
+
+        listView = (ListView) findViewById(R.id.listView);
+        CustomListViewAdapter adapter = new CustomListViewAdapter(this,
+                R.layout.main_listview, rowItems);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Item " + (position + 1) + ": " + rowItems.get(position),
+                Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+    }
+
+}
--- app/src/main/java/com/example/ouliviamikhael/onu/MainActivity.java
+++ app/src/main/java/com/example/ouliviamikhael/onu/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.ouliviamikhael.onu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import com.squareup.picasso.Picasso;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //set picasso image
+        /*ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(this)
+                .load("https://cms-assets.tutsplus.com/uploads/users/21/posts/19431/featured_image/CodeFeature.jpg")
+                .into(imageView);*/
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
